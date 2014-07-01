7f9007957195094c23cda3df887ec73df3583641
Robert Palmer <robisacommonusername@server.fake>
2014-07-01 22:55:58 +0800

    allowed choice between relative and absolute tolerance in assert_eq

--- assert_eq.m
+++ assert_eq.m
@@ -1,10 +1,26 @@
 function [status, msg] = assert_eq(a,b,varargin)
 	tol = 0.001;
+	type = 'rel';
 	if nargin > 2
 		tol = varargin{1};
 	end;
+	if nargin > 3
+		type = varargin{2};
+	end;
 	
-	if norm(a-b)/norm(a) < tol
+	err = norm(a-b)/norm(a);
+	switch (type)
+		case 'rel'
+		err = norm(a-b)/norm(a);
+		
+		case 'abs'
+		err = norm(a-b);
+		
+		otherwise
+		disp('WARNING: bad tolerance type, falling back to relative');
+	end;
+	
+	if err < tol
 		msg = 'PASS';
 		status = 0;
 	else
