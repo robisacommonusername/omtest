1679845c9897983ee71260dfebb346620cfcf35f
Robert Palmer <robisacommonusername@server.fake>
2014-12-17 15:02:39 +0800

    Added two new comparison modes for assert_eq

--- assert_eq.m
+++ assert_eq.m
@@ -12,19 +12,53 @@ function [status, msg] = assert_eq(a,b,varargin)
 	switch (type)
 		case 'rel'
 		err = norm(a-b)/norm(a);
+		if err < tol
+			msg = 'PASS';
+			status = 0;
+		else
+			msg = sprintf('FAIL. %s err=%f',type, err);
+			status = 1;
+		end;
 		
 		case 'abs'
 		err = norm(a-b);
+		if err < tol
+			msg = 'PASS';
+			status = 0;
+		else
+			msg = sprintf('FAIL. %s err=%f',type, err);
+			status = 1;
+		end;
+		
+		case 'pairwise_rel'
+		errors = abs(a-b)./abs(a);
+		%are any of the errors > tol?
+		num_errors = sum(errors > tol);
+		if num_errors > 0
+			status = 1;
+			msg = sprintf('FAIL: %d compraisons failed. Relative residuals were:\n',num_errors);
+			msg = strcat(msg, '[', sprintf('%f,\n',errors), ']\n');
+		else
+			status = 0;
+			msg = 'PASS';
+		end;
+		
+		case 'pairwise_abs'
+		errors = abs(a-b);
+		%are any of the errors > tol?
+		num_errors = sum(errors > tol);
+		if num_errors > 0
+			status = 1;
+			msg = sprintf('FAIL: %d compraisons failed. Residuals were:\n',num_errors);
+			msg = strcat(msg, '[', sprintf('%f,\n',errors), ']\n');
+		else
+			status = 0;
+			msg = 'PASS';
+		end;
 		
 		otherwise
 		disp('WARNING: bad tolerance type, falling back to relative');
 	end;
 	
-	if err < tol
-		msg = 'PASS';
-		status = 0;
-	else
-		msg = sprintf('FAIL. %s err=%f',type, err);
-		status = 1;
-	end;
+	
 end
