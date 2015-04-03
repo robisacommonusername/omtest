777ee1d360c58f6ad76d781f99689fea2a4ab16f
Robert Palmer <robisacommonusername@server.fake>
2015-04-03 10:33:55 +0800

    modified run_tests to allow re-running failed tests, etc

--- run_tests.m
+++ run_tests.m
@@ -1,5 +1,11 @@
-function ret = run_tests(handles, names, varargin)
+function ret = run_tests(tests, varargin)
 	STATUS_PASS = 0;
+	
+	%Old style - allow handles and test names to be passed separately
+	if nargin > 1
+		handles = tests
+		names = varargin{1};
+	end
 	
 	pass_i = 1;
 	fail_i = 1;
@@ -8,18 +14,29 @@ function ret = run_tests(handles, names, varargin)
 	messages = {};
 	
 	for ii = 1:length(handles)
-		f = handles{ii};
-		test_name = names{ii};
+		if nargin > 1
+			f = handles{ii};
+			test_name = names{ii};
+		else
+			f = tests(ii).f;
+			test_name = tests(ii).name;
+		end
+		
+		tic;
 		[status, msg] = f();
 		messages(ii).name = test_name;
 		messages(ii).text = msg;
 		if (status == STATUS_PASS)
 			passed(pass_i).name = test_name;
 			passed(pass_i).text = msg;
+			passed(pass_i).f = f;
+			passed(pass_i).time = toc;
 			pass_i = pass_i + 1;
 		else
 			failed(fail_i).name = test_name;
 			failed(fail_i).text = msg;
+			failed(fail_i).f = f;
+			failed(fail_i).time = toc;
 			fail_i = fail_i + 1;
 		end;
 	end;
